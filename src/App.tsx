92046ab556748125baccc490158703fab92ee185
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-06-20 09:23:04 -0500

    agregar contenedores principales

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import { menuItems } from "./data/db"
+import { menuItems } from "./data/db";
 
 function App() {
-  console.log(menuItems)
+  console.log(menuItems);
 
   return (
     <>
-     <header className="bg-teal-500 py-5">
-      <h1 className=" text-center text-4xl font-black">Calculadora de propinas y Consumos</h1>
-     </header>
+      <header className="bg-teal-500 py-5">
+        <h1 className=" text-center text-4xl font-black">
+          Calculadora de propinas y Consumos
+        </h1>
+      </header>
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menú</h2>{" "}
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
