ccc11ee0203929663f344ed1b3eddcc2510e55ae
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-07-04 15:43:45 -0500

    evitar duplicados en orden

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -5,9 +5,18 @@ export default function useOrder() {
     const[order,setOrder]=useState<OrderItem[]>([])
 
     const addItem =(item : MenuItem)=>{
+      const itemExist=order.find(orderItem =>orderItem.id === item.id)
+      if (itemExist){
+        const updateOrder = order.map( orderItem => orderItem.id===item.id ? {...orderItem, quantity: orderItem.quantity + 1} :orderItem)
 
-      const newItem : OrderItem = {...item, quantity:1}
+        setOrder(updateOrder)
+      
+      }else{
+        const newItem : OrderItem = {...item, quantity:1}
       setOrder ([...order, newItem])
+      }
+
+      
     } 
     console.log(order)
     
